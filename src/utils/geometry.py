4039f6e5533d9554130c4059093ff3552cf9c8be
FRANCESCO BORRELLI s337182 <s337182@studenti.polito.it>
2025-11-27 23:09:37 +0100

    Implement initial function stubs for geometry and visualization utilities

---
+++ src/utils/geometry.py
@@ -0,0 +1,9 @@
+def get_3d_corners():
+    # TODO: implement
+    
+def quaternion_to_matrix():
+    # TODO: implement
+    
+def project_points():
+    # TODO: implement
+    
---
+++ src/utils/visualization.py
@@ -0,0 +1,1 @@
+def draw_3d_box():
