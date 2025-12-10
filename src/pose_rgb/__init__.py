e64b1d262efabb441205dc56ce98d95413e19c4a
FRANCESCO BORRELLI s337182 <s337182@studenti.polito.it>
2025-12-10 20:23:02 +0100

    add the __init__

---
+++ src/pose_rgb/__init__.py
@@ -0,0 +1,4 @@
+from .dataset import LineModPoseDataset
+from .model import ResNetQuaternion
+from .pose_utils import convert_rotation_to_quaternion, quaternion_to_rotation_matrix
+from .test_dataset import test_dataset_basic, test_dataloader, test_all_objects, test_model_integration, test_quaternion_conversion, test_single_sample, run_all_tests
