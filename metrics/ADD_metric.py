fe6890ef0d5f6846b781f6c88a2107868182b50c
Federico Carollo <s299422@studenti.polito.it>
2025-12-17 12:37:45 +0100

    Feat: ADDed ADD-s metric for symmetric objects

--- metrics/ADD_metric.py
+++ metrics/ADD_metric.py
@@ -43,3 +43,53 @@ def compute_ADD_metric_quaternion(model_points, gt_quat, gt_translation,
     pred_rotation = quaternion_to_rotation_matrix(pred_quat)
     return compute_ADD_metric(model_points, gt_rotation, gt_translation,
                               pred_rotation, pred_translation)
+
+
+def compute_ADD_S_metric(model_points, gt_rotation, gt_translation,
+                         pred_rotation, pred_translation):
+    """
+    Compute ADD-S (Symmetric) metric for symmetric objects.
+    For each GT point, finds the closest predicted point.
+    
+    Args:
+        model_points: (N, 3) array of 3D model points in meters
+        gt_rotation: (3, 3) ground truth rotation matrix
+        gt_translation: (3,) ground truth translation vector in meters
+        pred_rotation: (3, 3) predicted rotation matrix
+        pred_translation: (3,) predicted translation vector in meters
+    
+    Returns:
+        add_s: Average closest point distance (ADD-S) metric in meters
+    """
+    # Transform model points with GT and prediction
+    gt_points = (gt_rotation @ model_points.T).T + gt_translation
+    pred_points = (pred_rotation @ model_points.T).T + pred_translation
+    
+    # For each GT point, find the closest predicted point
+    from scipy.spatial.distance import cdist
+    distances = cdist(gt_points, pred_points, metric='euclidean')
+    min_distances = distances.min(axis=1)
+    
+    add_s = min_distances.mean()
+    return add_s
+
+
+def compute_ADD_S_metric_quaternion(model_points, gt_quat, gt_translation,
+                                    pred_quat, pred_translation):
+    """
+    Compute ADD-S metric using quaternions (for symmetric objects).
+    
+    Args:
+        model_points: (N, 3) array of 3D model points
+        gt_quat: (4,) ground truth quaternion (w, x, y, z)
+        gt_translation: (3,) ground truth translation vector
+        pred_quat: (4,) predicted quaternion (w, x, y, z)
+        pred_translation: (3,) predicted translation vector
+    
+    Returns:
+        add_s: ADD-S metric
+    """
+    gt_rotation = quaternion_to_rotation_matrix(gt_quat)
+    pred_rotation = quaternion_to_rotation_matrix(pred_quat)
+    return compute_ADD_S_metric(model_points, gt_rotation, gt_translation,
+                                pred_rotation, pred_translation)
